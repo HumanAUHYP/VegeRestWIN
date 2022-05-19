4069751aa0b3b86406fde94337d61092d5206992
Ainur <ainur.hak2004@gmail.com>
2022-05-19 17:10:10 +0300

    enter client's info page append

--- CoreLibrary/Client.cs
+++ CoreLibrary/Client.cs
@@ -7,5 +7,12 @@ namespace CoreLibrary
         public string Name { get; set; }
         public string TableNum { get; set; }
         public Client() { }
+        public Client(string str)
+        {
+            var data = str.Split(';');
+            Id = int.Parse(data[0]);
+            Name = data[1];
+            TableNum = data[2];
+        }
     }
 }
