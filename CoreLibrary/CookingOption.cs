70e742d5e9da12902b6835ea906c94317663b426
Ainur <ainur.hak2004@gmail.com>
2022-05-23 17:01:30 +0300

    option storage

--- CoreLibrary/CookingOption.cs
+++ CoreLibrary/CookingOption.cs
@@ -4,9 +4,16 @@ using System.Text;
 
 namespace CoreLibrary
 {
-    class CookingOption
+    public class CookingOption
     {
         public int Id { get; set; }
         public string Title { get; set; }
+
+        public CookingOption(string str)
+        {
+            var data = str.Split(';');
+            Id = int.Parse(data[0]);
+            Title = data[1];
+        }
     }
 }
---
+++ CoreLibrary/OptionStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreLibrary
+{
+    public class OptionStorage
+    {
+        public List<CookingOption> CookingOptions { get; private set; }
+
+        public OptionStorage()
+        {
+            CookingOptions = new List<CookingOption>();
+            CookingOptions.Add()
+        }
+
+        public void Add(CookingOption cookingOption)
+        {
+            CookingOptions.Add(cookingOption);
+        }
+    }
+}
