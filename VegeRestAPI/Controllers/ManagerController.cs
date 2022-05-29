200e9c9270dce016306504b0aca5b65a852767de
Ainur <ainur.hak2004@gmail.com>
2022-05-29 11:58:59 +0300

    get menu and post menu pos

---
+++ VegeRestAPI/Controllers/ManagerController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using CoreLibrary;
+using System.Net;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace VegeRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+        private string menuPath = @"..\CoreLibrary\data\menu.txt";
+        private readonly MenuStorage menuStorage;
+
+        private readonly ILogger<ManagerController> _logger;
+        public ManagerController(ILogger<ManagerController> logger)
+        {
+            _logger = logger;
+            menuStorage = new MenuStorage();
+            menuStorage.ReadFromFile(menuPath);
+        }
+
+        // GET api/<ManagerController>/menu
+        [HttpGet]
+        public ActionResult<ICollection<Menu>> GetMenu()
+        {
+            return menuStorage.Menues;
+        }
+
+        // POST api/<ManagerController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Menu menu)
+        {
+            try
+            {
+                menuStorage.Add(menu);
+                menuStorage.WriteInFile(menuPath);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
--- VegeRestAPI/Startup.cs
+++ VegeRestAPI/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -6,9 +6,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+
 
 namespace VegeRestAPI
 {
@@ -25,11 +28,32 @@ namespace VegeRestAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
+            services.AddSwaggerGen(s =>
+                s.SwaggerDoc(
+                    "v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Version = "V1",
+                        Title = "VegeRest API",
+                        Description = "API for VegeRest"
+                    })
+                    );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "VegeRest API");
+                c.RoutePrefix = String.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
