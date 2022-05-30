4cd8080b91d492ab637c234ae7ef913ca8673669
Ainur <ainur.hak2004@gmail.com>
2022-05-30 10:16:14 +0300

    First unit test for menu

---
+++ VegeRestTests/MenuTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoreLibrary;
+
+namespace VegeRestTests
+{
+    [TestClass]
+    public class MenuTest
+    {
+        [TestMethod]
+        public void MenuConstructorTest()
+        {
+            Menu expectedMenu = new Menu
+            {
+                Id = 1,
+                Name = "menu",
+                Description = "descrip",
+                Image = "img",
+                Price = 2
+            };
+
+            Menu actualMenu = new Menu("1;menu;descrip;img;2");
+
+            Assert.AreEqual(expectedMenu.ToString(), actualMenu.ToString());
+        }
+
+        [TestMethod]
+        public void MenuStorageAddTest()
+        {
+            var menuStorage = new MenuStorage();
+            var menu1 = new Menu("1;menu1;descrip;img;2");
+            var menu2 = new Menu("2;menu2;descrip;img;2");
+            var menu3 = new Menu("3;menu3;descrip;img;2");
+            menuStorage.Add(menu1);
+            menuStorage.Add(menu2);
+            menuStorage.Add(menu3);
+
+
+        }
+    }
+}
