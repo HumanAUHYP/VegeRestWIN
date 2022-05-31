dda9628c215cdfc4cbcdc30053fa2afb41543b67
Ainur <ainur.hak2004@gmail.com>
2022-05-31 22:30:23 +0300

    MSTests all

--- VegeRestTests/MenuTest.cs
+++ VegeRestTests/MenuTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoreLibrary;
+using System.Collections.Generic;
 
 namespace VegeRestTests
 {
@@ -27,14 +28,79 @@ namespace VegeRestTests
         public void MenuStorageAddTest()
         {
             var menuStorage = new MenuStorage();
+
             var menu1 = new Menu("1;menu1;descrip;img;2");
             var menu2 = new Menu("2;menu2;descrip;img;2");
             var menu3 = new Menu("3;menu3;descrip;img;2");
+
+            var listMenu = new List<Menu>{menu1, menu2, menu3};
+
             menuStorage.Add(menu1);
             menuStorage.Add(menu2);
             menuStorage.Add(menu3);
 
+            CollectionAssert.AreEqual(menuStorage.Menues, listMenu);
+        }
 
+        [TestMethod]
+        public void MenuStorageRemoveTest()
+        {
+            var menuStorage = new MenuStorage();
+
+            var menu1 = new Menu("1;menu1;descrip;img;2");
+            var menu2 = new Menu("2;menu2;descrip;img;2");
+            var menu3 = new Menu("3;menu3;descrip;img;2");
+
+            var listMenu = new List<Menu> { menu1, menu2 };
+
+            menuStorage.Add(menu1);
+            menuStorage.Add(menu2);
+            menuStorage.Add(menu3);
+
+            menuStorage.RemoveById("3");
+
+            CollectionAssert.AreEqual(menuStorage.Menues, listMenu);
+        }
+
+        [TestMethod]
+        public void MenuStorageChangeTest()
+        {
+            var menuStorage = new MenuStorage();
+
+            var menu1 = new Menu("1;menu1;descrip;img;2");
+            var menu2 = new Menu("2;menu2;descrip;img;2");
+            
+            menuStorage.Add(menu1);
+            menuStorage.Add(menu2);
+
+            menu2.Price = 10;
+            menu2.Name = "changedMenu2";
+            menu2.Description = "changedDescrip";
+            menu2.Image = "changedImg";
+
+            var listMenu = new List<Menu> { menu1, menu2 };
+
+            menuStorage.Change(menu2);
+
+            CollectionAssert.AreEqual(menuStorage.Menues, listMenu);
+        }
+
+        [TestMethod]
+        public void MenuStorageWorkWithFileTest()
+        {
+            var path = "test.txt";
+
+            var menuStorage = new MenuStorage();
+
+            var menu1 = new Menu("1;menu1;descrip;img;2");
+
+            menuStorage.Add(menu1);
+
+            menuStorage.WriteInFile(path);
+            menuStorage.Menues.Clear();
+            menuStorage.ReadFromFile(path);
+
+            Assert.AreEqual(menuStorage.Menues[0].ToString(), menu1.ToString());
         }
     }
 }
---
+++ VegeRestTests/OrderTest.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoreLibrary;
+using System.Collections.Generic;
+
+namespace VegeRestTests
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void OrderConstructorTest()
+        {
+            ClientStorage.Add(new Client("1;client;10"));
+            var expectedOrder = new Order
+            {
+                Id = 1,
+                Name = "order",
+                ClientId = 1,
+                CookingOption = CookingOptions.Options[0].Title,
+                CuttingOption = CuttingOptions.Options[0].Title,
+                Count = 5
+            };
+
+            Order actualOrder = new Order("1;order;10;Не готовить;Не нарезать;5");
+
+            Assert.AreEqual(expectedOrder.ToString(), actualOrder.ToString());
+        }
+
+        [TestMethod]
+        public void OrderStorageAddTest()
+        {
+            ClientStorage.Add(new Client("1;client;10"));
+            var orderStorage = new OrderStorage();
+
+            var order1 = new Order("1;order1;10;Не готовить;Не нарезать;5");
+            var order2 = new Order("2;order2;10;Не готовить;Не нарезать;5");
+            var order3 = new Order("3;order3;10;Не готовить;Не нарезать;5");
+            
+
+            var listOrder = new List<Order> { order1, order2, order3 };
+
+            orderStorage.Add(order1);
+            orderStorage.Add(order2);
+            orderStorage.Add(order3);
+
+            CollectionAssert.AreEqual(orderStorage.Orders, listOrder);
+        }
+
+        [TestMethod]
+        public void OrderStorageRemoveTest()
+        {
+            var orderStorage = new OrderStorage();
+
+            var order1 = new Order("1;order1;10;Не готовить;Не нарезать;5");
+            var order2 = new Order("2;order2;10;Не готовить;Не нарезать;5");
+            var order3 = new Order("3;order3;10;Не готовить;Не нарезать;5");
+
+            var listOrder = new List<Order> { order1, order2 };
+
+            orderStorage.Add(order1);
+            orderStorage.Add(order2);
+            orderStorage.Add(order3);
+
+            orderStorage.RemoveById("3");
+
+            CollectionAssert.AreEqual(orderStorage.Orders, listOrder);
+        }
+
+        [TestMethod]
+        public void OrderStorageChangeTest()
+        {
+            var orderStorage = new OrderStorage();
+
+            var order1 = new Order("1;order1;10;Не готовить;Не нарезать;5");
+            var order2 = new Order("2;order2;10;Не готовить;Не нарезать;5");
+
+            orderStorage.Add(order1);
+            orderStorage.Add(order2);
+
+            order2.Price = 10;
+            order2.Name = "changedOrder2";
+            order2.CookingOption = "changedCookingOption";
+            order2.CuttingOption = "changedCuttingOption";
+            order2.Count = 8;
+
+            var listOrder = new List<Order> { order1, order2 };
+
+            orderStorage.Change(order2);
+
+            CollectionAssert.AreEqual(orderStorage.Orders, listOrder);
+        }
+
+        [TestMethod]
+        public void OrderStorageWorkWithFileTest()
+        {
+            var path = "test.txt";
+
+            var orderStorage = new OrderStorage();
+
+            var order1 = new Order("1;order1;10;Не готовить;Не нарезать;5");
+
+            orderStorage.Add(order1);
+
+            orderStorage.WriteInFile(path);
+            orderStorage.Orders.Clear();
+            orderStorage.ReadFromFile(path);
+
+            Assert.AreEqual(orderStorage.Orders[0].ToString(), order1.ToString());
+        }
+    }
+}
